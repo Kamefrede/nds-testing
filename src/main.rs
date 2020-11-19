1c280cc270d5566a5d9c63ce1eaa6cd2f3400797
Kamefrede <kamefrede@gmail.com>
2020-11-19 20:41:04 +0000

    Working Hello World example

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+#![no_std]
+#![no_main]
+#![feature(lang_items)]
+#![feature(start)]
+
+use nds_sys;
+use nds_sys::{consoleDemoInit, iprintf, consoleDebugInit, PrintConsole, consoleClear, printf};
+use core::panic::PanicInfo;
+
+macro_rules! c_str {
+    ($s:expr) => {{
+        concat!($s, "\0").as_ptr()
+    }};
+}
+
+#[no_mangle]
+#[start]
+fn main(argc: isize, argv: * const* const u8) -> isize {
+    unsafe {
+        consoleDemoInit();
+        printf(c_str!("Hello, from Rust!"));
+        loop {
+        }
+    }
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
